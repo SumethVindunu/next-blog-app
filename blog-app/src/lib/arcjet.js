831dce228db04751bb9fcd3c7f3c55926f185b9a
Sumeth Vindunu <164194470+SumethVindunu@users.noreply.github.com>
2025-01-18 22:20:17 +0530

    second-day

--- blog-app/src/lib/arcjet.js
+++ blog-app/src/lib/arcjet.js
@@ -1,9 +1,31 @@
-import arcjet from "@arcjet/next";
+import arcjet,{protectSignup} from "@arcjet/next";
 
 
 const aj = arcjet({
     key:process.env.ARCJET_KEY,
-    rules : [],
-});
+    rules : [
+        
+        //protect sign up form
+    protectSignup({
+        email: {
+          mode: "LIVE",
+          block: ["DISPOSABLE", "INVALID", "NO_MX_RECORDS"],
+        },
+        bots: {
+          mode: "LIVE",
+          allow: [],
+        },
+        rateLimit: {
+          mode: "LIVE",
+          interval: "1m",
+          max: 50,
+        },
+      }),
+    ],
+    
+        
+    })
+    
+
 
 export default aj;
